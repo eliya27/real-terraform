02a3494ca3c22c57ddd5a27584ed48d1e0498ca3
Eliya Gervas <eliyagervas27@gmail.com>
2022-05-24 10:00:29 +0300

    updates

--- main.tf
+++ main.tf
@@ -29,9 +29,9 @@ provider "azuread" {
 
 
 
-#module "aks" {
-#   source = "./az-modules/aks"
-#}
+module "aks" {
+   source = "./az-modules/aks"
+}
 
 module "acr" {
    source = "./az-modules/acr"
